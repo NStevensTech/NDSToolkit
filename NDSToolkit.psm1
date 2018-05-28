9a99be60eb7713c37bb926e3b1daa2ccf28a5a72
Nathan Stevens <mr.bloopbloop@gmail.com>
2018-05-28 13:59:48 -0500

    Add files via upload

---
+++ NDSToolkit.psm1
@@ -0,0 +1,11 @@
+﻿$Module = @(Get-ChildItem -Path $PSScriptRoot\cmdlets\*.ps1 -ErrorAction SilentlyContinue)
+
+foreach ($item in $Module) {
+    Try{
+        .$item.fullname
+    } Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+Export-ModuleMember -Function *
